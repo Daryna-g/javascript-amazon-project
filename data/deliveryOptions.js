b1958c7cb456a8a315d76a3b31e151afb8a04ad0
Daryna-g <gickad@gmail.com>
2025-06-21 17:17:01 +0300

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -9,5 +9,17 @@ export const deliveryOptions = [{
 }, {
 	id: '3',
 	deliveryDays: 1,
-	priceCence: 999
+	priceCents: 999
 }];
+
+export function getDeliveryOption(deliveryOptionId) {
+	let deliveryOption;
+
+	deliveryOptions.forEach((option) => {
+		if (option.id === deliveryOptionId) {
+			deliveryOption = option;
+		}
+	});
+
+	return deliveryOption || deliveryOptions[0];
+}
